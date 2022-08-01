3c356b5a2ba54c319654a5281669c55a223aed94
tvstditf <tvstditfstorage@gmail.com>
2022-08-01 14:38:59 +0100

    return res changes

--- routes/centre.js
+++ routes/centre.js
@@ -150,9 +150,10 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
     const centre = await newCentre.save();
     console.log(centre);
 
-    res.status(200).json(centre);
+    return res.status(200).json(centre);
   } catch (error) {
-    res.status(500).json(error);
+    console.log(error);
+    return res.status(500).json(error);
   }
 });
 
